9de23de72b11f8518ac60e4bc5797cd60d53e370
JustZvan <112730082+JustZvan@users.noreply.github.com>
2025-08-22 13:13:44 +0200

    feat: add building into exe via cxFreeze and Inno based installer

---
+++ build.py
@@ -0,0 +1,15 @@
+from cx_Freeze import setup
+
+# Dependencies are automatically detected, but they might need fine-tuning.
+build_exe_options = {
+    "excludes": ["tkinter", "unittest"],
+    "zip_include_packages": ["PySide6"],
+}
+
+setup(
+    name="GWEM",
+    version="0.1",
+    description="My GUI application!",
+    options={"build_exe": build_exe_options},
+    executables=[{"script": "main.py", "base": "gui"}],
+)
